0107a23bd73a781a07e940efc666fa7465db84a8
mr.samani <mr.samani1368@gmail.com>
2026-09-23 18:31:54 +0330

    fixup! release 8.0.0

--- projects/libs/side-nav/public-api.ts
+++ projects/libs/side-nav/public-api.ts
@@ -1,3 +1,9 @@
 export * from './ngx-side-nav.module';
 
 export * from './contracts/mode';
+
+
+export * from './directives/drawer-menu';
+export * from './directives/drawer-menu-toggle';
+
+export * from './components/side-nav/side-nav';
