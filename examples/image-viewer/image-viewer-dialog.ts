0420a7b6af1bba2f3bab56aeb0af056b2ea4b963
mr-samani <mr-samani@users.noreply.github.com>
2026-09-23 04:46:45 +0000

    deploy: c51e8c4b82a239dfeb7092e4678b3e7edebb8a78

--- examples/image-viewer/image-viewer-dialog.ts
+++ examples/image-viewer/image-viewer-dialog.ts
@@ -1,11 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { DIALOG_DATA, DIALOG_REF } from 'ngx-kit/dialog';
-import { NgxImageViewer, type NgxImageViewerItem } from 'ngx-kit/image-viewer';
+import {
+  NgxImageViewer,
+  NgxImageViewerItem,
+  NgxImageViewerToolbarConfig,
+} from 'ngx-kit/image-viewer';
 
 @Component({
   template: `
-    <ngx-image-viewer [images]="data.images" />
-    <button type="button" (click)="close()" class="close-btn">X</button>
+    <ngx-image-viewer [images]="data.images" [toolbar]="toolbar" (closed)="close()" />
   `,
   imports: [NgxImageViewer],
   styles: `
@@ -13,19 +16,12 @@ import { NgxImageViewer, type NgxImageViewerItem } from 'ngx-kit/image-viewer';
       display: block;
       height: 100%;
     }
-    .close-btn {
-      position: fixed;
-      top: 1rem;
-      right: 1rem;
-      background: none;
-      border: none;
-      outline: none;
-      font-family: cursive;
-      font-size: 1.2rem;
-    }
   `,
 })
 export class ImageViewDialog {
+  toolbar: NgxImageViewerToolbarConfig = {
+    close: true,
+  };
   readonly data = inject<{ images: NgxImageViewerItem[] }>(DIALOG_DATA);
   protected readonly dialogRef = inject(DIALOG_REF);
   close() {
